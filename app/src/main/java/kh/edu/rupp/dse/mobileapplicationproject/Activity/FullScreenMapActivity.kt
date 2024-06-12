197f94a46bac9dc223fad19bb9c1531be7a15a73
LengKoemHort <133197438+KoemPark@users.noreply.github.com>
2024-06-12 10:03:01 +0700

    last

---
+++ app/src/main/java/kh/edu/rupp/dse/mobileapplicationproject/Activity/FullScreenMapActivity.kt
@@ -0,0 +1,12 @@
+package kh.edu.rupp.dse.mobileapplicationproject.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kh.edu.rupp.dse.mobileapplicationproject.R
+
+class FullScreenMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_map)
+    }
+}
